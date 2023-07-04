759eee9a7a0bda985a2fd6511f7c736b3afd8b46
aybek <aybek.mamasaliev@bk.ru>
2023-07-04 16:23:07 +0600

    feat

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/products';
+import { ProductService } from './services/product.service';
+import { Observable, tap } from 'rxjs';
+import { ModalService } from './services/modal.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+}
---
+++ src/app/pages/product-page/product-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.scss']
+})
+export class ProductPageComponent implements OnInit{
+  title = 'angular crash course';
+  // products: IProduct[]=[]
+  loading = false
+  // products$: Observable<IProduct[]>
+  term = ""
+
+  constructor(public productsService: ProductService, 
+    public modalService: ModalService
+    ){
+  }
+
+  ngOnInit(): void {
+    this.loading=true
+    // this.products$ = this.productsService.getAll().pipe(
+    //   tap(()=>this.loading=false)
+    // )
+    this.productsService.getAll().subscribe(()=>{
+        this.loading=false
+    })
+  }
+}
